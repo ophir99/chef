1373babef25fff35e0fbd8ba5114ee9dc4aa6fda
Ophir Raj <ophirraj@Ophirs-MacBook-Air.local>
2018-05-22 00:33:33 +0530

    Login and Reg is fixed

--- user/user.js
+++ user/user.js
@@ -10,27 +10,59 @@ const userSchema = new mongoose.Schema({
 });
 const UserModel =  mongoose.model('UserModel', userSchema);
 module.exports.user={
-     Save: function(req){
+     Save: function(req,x){
         console.log(req.body);
-        let userx = new UserModel({
-            fname: req.body.fname,
-            lname: req.body.lname,
-            user_name: req.body.user_name,
-            email: req.body.email,
-            password: req.body.password,
-        });
-        return userx.save()
-     },
-     login: function(req){
-           let email =  req.body.email;
-           let password = req.body.password
-           return UserModel.findOne({email,password}, '_id', function(err,data){
+        UserModel.find({email: req.body.email}, function(err,res){
             if(err){
-                return 'Login Failed!!. Try Again';
+                console.log("err",err);
+                x.send(res);
             }
-            if(data){
-                return 'Okay'
+            if(res){
+                if(res.length === 0){
+                    let userx = new UserModel({
+                        fname: req.body.fname,
+                        lname: req.body.lname,
+                        user_name: '',
+                        email: req.body.email,
+                        date: new Date(),
+                        password: req.body.password,
+                    });
+                    userx.save(function(err,data){
+                        if(err){
+                                x.send({'msg': 'Unable to Save Try, again'})
+                        }
+                        if(data){
+                            x.send({'msg':data});
+                        }
+                    });
+                }
+                else{
+                    x.send({'msg': 'Email is already used. Try with other'});
+                }
             }
-           });
+        });
+        
+     },
+     login: function(req,x){
+           let email_ =  req.body.email;
+           let password_ = req.body.password
+           console.log("Request Body",req.body)
+           /*return UserModel.findOne({email:email_}).where('email').equals(email_)
+           .where('password').equals(password_).select('_id user_name')*/
+
+            UserModel.find({email: email_, password: password_}, function(err,res){
+                if(err){
+                    console.log("err",err);
+                    x.send(res);
+                }
+                if(res){
+                    if(res.length === 0){
+                        x.send({'msg': 'Username not found'});
+                    }
+                    else{
+                        x.send({'msg': 'Logged In'});
+                    }
+                }
+            });
      }
 }
